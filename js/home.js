c97348d2ca7790f6b7e68a15ea59c9030d25ea22
Vishnu <vishnun@thoughtworks.com>
2015-06-27 16:46:52 +0530

    Vishnu: Adding a fancy menu hamburger icon.

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){document.getElementById("intro").style.height=window.innerHeight-20+"px",$(".loader-overlay").hide();var e=new WOW({boxClass:"wow",animateClass:"animated",offset:20,mobile:!0,live:!0});e.init();var n=$("#js-mobile-menu").unbind();$("#js-navigation-menu").removeClass("show"),n.on("click",function(e){e.preventDefault();var n=$("#js-navigation-menu");n.slideToggle(function(){n.is(":hidden")&&n.removeAttr("style")})});var i=function(){var e=0,n=$(".evenHeights");n.each(function(){var n=$(this);n.height()>e&&(e=n.height())}),n.height(e)};i()});
+$(document).ready(function(){function e(){1==s?(o.removeClass("is-closed"),o.addClass("is-open"),s=!1):(o.removeClass("is-open"),o.addClass("is-closed"),s=!0)}document.getElementById("intro").style.height=window.innerHeight-20+"px",$(".loader-overlay").hide();var n=new WOW({boxClass:"wow",animateClass:"animated",offset:20,mobile:!0,live:!0});n.init();var i=$("#js-mobile-menu").unbind();$("#js-navigation-menu").removeClass("show"),i.on("click",function(e){e.preventDefault();var n=$("#js-navigation-menu");n.slideToggle(function(){n.is(":hidden")&&n.removeAttr("style")})});var o=$("#hamburger"),s=!0;o.click(function(){e()});var t=function(){var e=0,n=$(".evenHeights");n.each(function(){var n=$(this);n.height()>e&&(e=n.height())}),n.height(e)};t()});
 
 //$(document).ready(function () {
 //
@@ -20,6 +20,30 @@ $(document).ready(function(){document.getElementById("intro").style.height=windo
 //      }
 //    });
 //  });
+//
+//  var trigger = $('#hamburger'),
+//    isClosed = true;
+//
+//  function burgerTime() {
+//    if (isClosed == true) {
+//      trigger.removeClass('is-closed');
+//      trigger.addClass('is-open');
+//      isClosed = false;
+//    } else {
+//      trigger.removeClass('is-open');
+//      trigger.addClass('is-closed');
+//      isClosed = true;
+//    }
+//  }
+//
+//
+//  trigger.click(function () {
+//    burgerTime();
+//  });
+//
+//
+//
+//
 //  var setEvenHeights = function () {
 //    var maxHeight = 0;
 //    var $evenHeightElements = $('.evenHeights');
