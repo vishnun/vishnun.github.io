f6aec7b3ed7b675cfe6b5f85b5bb40f8fa718d69
Vishnu <vishnun@thoughtworks.com>
2015-06-27 16:02:03 +0530

    Vishnu: Optimizing stuff related to font and all. Adding a loader image as well.

--- js/home.js
+++ js/home.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){var e=new WOW({boxClass:"wow",animateClass:"animated",offset:20,mobile:!0,live:!0});e.init();var n=$("#js-mobile-menu").unbind();$("#js-navigation-menu").removeClass("show"),n.on("click",function(e){e.preventDefault();var n=$("#js-navigation-menu");n.slideToggle(function(){n.is(":hidden")&&n.removeAttr("style")})});var i=function(){var e=0,n=$(".evenHeights");n.each(function(){var n=$(this);n.height()>e&&(e=n.height())}),n.height(e)};i()});
+$(document).ready(function(){document.getElementById("intro").style.height=window.innerHeight-20+"px",$(".loader-overlay").hide();var e=new WOW({boxClass:"wow",animateClass:"animated",offset:20,mobile:!0,live:!0});e.init();var n=$("#js-mobile-menu").unbind();$("#js-navigation-menu").removeClass("show"),n.on("click",function(e){e.preventDefault();var n=$("#js-navigation-menu");n.slideToggle(function(){n.is(":hidden")&&n.removeAttr("style")})});var i=function(){var e=0,n=$(".evenHeights");n.each(function(){var n=$(this);n.height()>e&&(e=n.height())}),n.height(e)};i()});
+
 //$(document).ready(function () {
+//
+//  document.getElementById("intro").style.height = (window.innerHeight-20)+'px';
+//
+//  $('.loader-overlay').hide();
+//
 //  var wow = new WOW({boxClass: 'wow', animateClass: 'animated', offset: 20, mobile: true, live: true});
 //  wow.init();
+//
 //  var menuToggle = $('#js-mobile-menu').unbind();
 //  $('#js-navigation-menu').removeClass("show");
 //  menuToggle.on('click', function (e) {
